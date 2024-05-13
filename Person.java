76220150f2b71dad2be3a8e97ba79d274129bdd6
LAMI <tolboni614@gmail.com>
2024-05-13 20:04:15 +0300

    How to get and set name in java

---
+++ Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private String name;
+    private int age;
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter for age
+    public int getAge() {
+        return age;
+    }
+}
