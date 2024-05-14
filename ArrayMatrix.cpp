7ac47837157e8c80b065227bcac153208227601a
LAMI <tolboni614@gmail.com>
2024-05-14 20:02:02 +0300

    2D array

--- ArrayMatrix.cpp
+++ ArrayMatrix.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     string students[10][5];
-    string columns[5]={" Name:","Father Name:","Last Name:","Idno:","Gender:"};
+    string columns[5]={" Frist Name:","Father Name:","Last Name:","Idno:","Gender:"};
     for(int i=0;i<10;i++)
     for(int j=0;j<5;j++){
         cout<<"Enter student "<<i+1<<" "<<columns[j];
